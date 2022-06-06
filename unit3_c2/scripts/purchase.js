5fa430f9d2e2cafb1afdcf494e5129eeb27ff5db
ingole8888 <ingole8888@gmail.com>
2022-06-06 17:50:05 +0530

    sss

--- unit3_c2/scripts/purchase.js
+++ unit3_c2/scripts/purchase.js
@@ -1,4 +1,31 @@
+function balance(){
+    
+    let wallet_balance=document.getElementById('wallet_balance')
+    wallet_balance.innerText=JSON.parse(localStorage.getItem('user')).wallet;
+  }
+  balance()
+let vouchers=JSON.parse(localStorage.getItem('purchase'))
+  function append(vouchers){
+    console.log(vouchers)
+    let container=document.getElementById('purchased_vouchers')
+    vouchers.forEach(function(el){
+      let div=document.createElement('div')
+       
+         let img=document.createElement('img')
+         img.src=el.image;
+         
+         let title=document.createElement('h2')
+         title.innerText=el.name;
 
+         let price=document.createElement('h3')
+         price.innerText=el.price;
+       div.append(img,title,price)
+       container.append(div)
+    })
+
+  }
+  append(vouchers)
+    
 
 
 
--- unit3_c2/scripts/voucher.js
+++ unit3_c2/scripts/voucher.js
@@ -1,51 +1,61 @@
-//let amount = JSON.parse(localStorage.getItem("amount")) || 0;
-//document.getElementById("wallet_balance").innerText=amount;
+var user=JSON.parse(localStorage.getItem('user'))
+  let purchasedVouchers=JSON.parse(localStorage.getItem('purchase')) || []
+  function balance(){
+    
+    let wallet_balance=document.getElementById('wallet_balance')
+    wallet_balance.innerText=JSON.parse(localStorage.getItem('user')).wallet;
+  }
+balance()
+  async function showVoucher(){
+    try {
+      let res=await fetch('https://masai-vouchers-api.herokuapp.com/api/vouchers')
+      let data=await res.json()
+      append(data[0].vouchers)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  showVoucher()
+  function append(data){
+    //console.log(data)
+    let container=document.getElementById('voucher_list')
+    data.forEach(function(el){
+         let div=document.createElement('div')
+         div.setAttribute('class','voucher')
+         let img=document.createElement('img')
+         img.src=el.image;
+         
+         let title=document.createElement('h2')
+         title.innerText=el.name;
 
+         let price=document.createElement('h3')
+         price.innerText=el.price;
 
-let url="https://masai-vouchers-api.herokuapp.com/api/vouchers";
-
-fetch(url)
-.then(function(res){
-    return res.json();
-})
-.then(function(res){
-    let data = res;
-    append(data)
-    console.log(data);
-})
-.catch(function(err){
-    console.log(err);
-})
-
-function append(data){
-    var container=document.getElementById("voucher_list");
-    container.innerHTML=null;
-    data.map(function(el){
-        let div=document.createElement("div");
-        div.setAttribute("class","voucher");
-
-        let name =document.createElement("h3");
-        name.innerText=el.name;
-
-        let img =document.createElement("img");
-        img.src=el.image;
-
-        let price =document.createElement("h3");
-        price.innerText=el.price;
-
-        let btn=document.createElement("button");
-        btn.setAttribute("id","buy_voucher");
-        btn.addEventListener("click",function(){
-            show(el);
-        })
-        btn.innerText="Buy Voucher";
-        div.append(name,img,price,btn);
-        //var container=document.getElementById("voucher_list");
-        container.append(div);
-        console.log(div);
+         let button=document.createElement('button')
+         button.innerText='Buy'
+         button.setAttribute('class','buy_voucher')
+         button.addEventListener('click',function(){
+               selected(el)
+         })
+         div.append(img,title,price,button)
+         container.append(div)
     })
-}
-function show(el){
-    window.location="purchase.html";
-    localStorage.setItem("purchase",JSON.stringify(el));
-}
+  }
+  function selected(el){
+     let balance_value=Number(user.wallet)-Number(el.price)
+     //console.log(balance_value)
+     if(balance_value>=0){
+       alert('Hurray! purchase successful')
+       console.log(el)
+       console.log(purchasedVouchers)
+       purchasedVouchers.push(el)
+     user.wallet=balance_value;
+     localStorage.setItem('purchase',JSON.stringify(purchasedVouchers))
+     localStorage.setItem('user',JSON.stringify(user))
+     balance()
+     }
+     else if(balance_value<0){
+       alert('Sorry! insufficient balance')
+     }
+  }
